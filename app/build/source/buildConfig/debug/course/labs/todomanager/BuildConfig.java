ea882917b21b81bac41b523eb94b80cd32590f11
Mário Melo <mariofdemelo@gmail.com>
2014-05-07 18:59:22 -0300

    Commit inicial

---
+++ app/build/source/buildConfig/debug/course/labs/todomanager/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package course.labs.todomanager;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "course.labs.todomanager";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
