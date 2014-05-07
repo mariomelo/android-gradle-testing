519e14621029d89a390e1053da918708c971c3c5
Mário Melo <mariofdemelo@gmail.com>
2014-05-07 19:46:08 -0300

    Realiza testes unitarios

---
+++ app/src/test/java/com/gotoagile/csd/SimpleTest.java
@@ -0,0 +1,18 @@
+package com.gotoagile.csd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import course.labs.todomanager.ToDoItem;
+
+public class SimpleTest {
+
+    @Test
+    public void doesNotExplode() throws Exception {
+        ToDoItem todoItem = new ToDoItem("Titulo!", null, null, null);
+        assertNotNull(todoItem.getTitle());
+        assertNull(todoItem.getTitle());
+        assertNull(todoItem.getPriority());
+    }
+}
